9eb5b6a807051600750aceb23f55b60938713465
kirthiSaiT <kirthisai251@gmail.com>
2024-08-03 23:35:07 +0530

    added the base outline

---
+++ index.js
@@ -0,0 +1,25 @@
+class Game{
+    constructor(){}
+        Start(){
+            console.log("game started")
+        }
+    
+}
+
+class Snake{
+    constructor(){}
+        
+    
+}
+
+
+class Food{
+    constructor(){}
+
+    
+}
+
+document.getElementById("start").addEventListener("click",()=>{
+    const game=new Game()
+    game.Start();
+})
